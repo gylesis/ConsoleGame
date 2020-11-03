e6099069276802d89ff770cb5e6729e86cb95a9e
Nikita <nikitadedov66@gmail.com>
2020-11-04 01:28:40 +0200

    main

--- ConsoleGame/ConsoleGame/Program.cs
+++ ConsoleGame/ConsoleGame/Program.cs
@@ -1,9 +1,58 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace ConsoleGame {
+namespace Game {
+
     class Program {
+
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+            char player = '@';
+            int x = 3;
+            int y = 3;
+
+            #region Map
+            List<string> map = new List<string> { };
+            map.Add("█████████████████████████████");
+            map.Add("█                           █");
+            map.Add("█                           █");
+            map.Add("█                           █");
+            map.Add("█                           █");
+            map.Add("█                           █");
+            map.Add("█                           █");
+            map.Add("█                           █");
+            map.Add("█                           █");
+            map.Add("█                           █");
+            map.Add("█                           █");
+            map.Add("█                           █");
+            map.Add("█                           █");
+            map.Add("█                           █");
+            map.Add("█                           █");
+            map.Add("█                           █");
+            map.Add("█                           █");
+            map.Add("█████████████████████████████");
+
+            foreach (string partOfMap in map) {
+                Console.WriteLine(partOfMap);
+            }
+            #endregion
+
+            while (true) {
+                Console.Clear();
+                foreach (string partOfMap in map) {
+                    Console.WriteLine(partOfMap);
+                }
+                Console.SetCursorPosition(x, y);
+                Console.OpenStandardOutput();
+                Console.Write(player);
+                ConsoleKeyInfo Input = Console.ReadKey();
+                if (Input.KeyChar == 'w') y -= 1;
+                if (Input.KeyChar == 'a') x -= 1;
+                if (Input.KeyChar == 's') y += 1;
+                if (Input.KeyChar == 'd') x += 1;
+            }
+
+
         }
     }
 }
